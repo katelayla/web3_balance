2f176565f97fc601bfde83341a68e3238d2ecb07
Kate <118492731+katelayla@users.noreply.github.com>
2023-06-16 14:19:58 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from time import sleep, time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
